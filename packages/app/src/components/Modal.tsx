e8c41c74f0319c4e5654854b12722166cee708fa
edsonalcala <edson_alcala@msn.com>
2023-12-14 13:13:59 +0100

    remove comment

--- packages/app/src/components/Modal.tsx
+++ packages/app/src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import { useAccount, useDisconnect } from 'wagmi'; // import your authentication library here
+import { useAccount, useDisconnect } from 'wagmi';
 
 interface Props {
     open: boolean;
@@ -19,16 +19,14 @@ export default function ({ open, setOpen }: Props) {
                 } fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity`}
             aria-labelledby="modal-title"
             role="dialog"
-            aria-modal="true"
-        >
+            aria-modal="true">
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <div
                         className={`relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6 ${open
                             ? 'opacity-100 translate-y-0 sm:scale-100'
                             : 'opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-                            }`}
-                    >
+                            }`}>
                         <div>
                             <div className="mt-3 text-center sm:mt-5">
                                 <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">
